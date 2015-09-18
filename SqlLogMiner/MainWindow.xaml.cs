e291c3c6bdbad6e5a4a55972aff13afdb0535023
msalguero <msalguero@unitec.edu>
2015-09-17 21:57:16 -0500

    Open File Dialog and Save File Dialog

--- SqlLogMiner/MainWindow.xaml.cs
+++ SqlLogMiner/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace SqlLogMiner
 {
@@ -20,9 +21,41 @@ namespace SqlLogMiner
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string SavePath { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+
+            SavePath = "";
+        }
+
+        private void New(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Open(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                
+            }
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            if(SavePath == "")
+                SaveAs(sender, e);
+        }
+
+        private void SaveAs(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                SavePath = saveFileDialog.FileName;
+            }
         }
     }
 }
